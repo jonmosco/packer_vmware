898623efa1c77f7629f3530285c7ab4961ec6559
Jon Mosco <jonny.mosco@gmail.com>
2014-10-01 13:57:31 -0400

    fixed networking.  Release ready!

--- scripts/network.sh
+++ scripts/network.sh
@@ -1,16 +1,18 @@
 #!/bin/sh
 # Make networking work!
 
-IFCONFIG=`cat <<-END
-DEVICE="eth0"
-NM_CONTROLLED="no"
-ONBOOT="yes"
-BOOTPROTO="dhcp"
-`
+#IFCONFIG=`cat <<-END
+#DEVICE="eth0"
+#NM_CONTROLLED="no"
+#ONBOOT="yes"
+#BOOTPROTO="dhcp"
+#`
+#
+#UDEV=`cat <<-END
+#SUBSYSTEM=="net", ACTION=="add", DRIVERS="?*", ATTR{type}=="1", KERNEL=="eth*", NAME="eth0"
+#`
+#
+#echo "$IFCONFIG" > /etc/sysconfig/network-scripts/ifcfg-eth0
+#echo "$UDEV" > /etc/udev/rules.d/70-persistent-net.rules
 
-UDEV=`cat <<-END
-SUBSYSTEM=="net", ACTION=="add", DRIVERS="?*", ATTR{type}=="1", KERNEL=="eth*", NAME="eth0"
-`
-
-echo "$IFCONFIG" > /etc/sysconfig/network-scripts/ifcfg-eth0
-echo "$UDEV" > /etc/udev/rules.d/70-persistent-net.rules
+rm -f /etc/udev/rules.d/70-persistent-net.rules
